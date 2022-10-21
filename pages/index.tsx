a3554b9c25f6e2fb4520dad821ba7a7f82e8e7af
uris <urialbertogalvafrancisco@gmail.com>
2022-10-21 17:44:44 -0400

    wip config eslinter and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <h1>Pokemon-static-app</h1>
-  )
+  return <h1></h1>
 }
 
 export default Home
