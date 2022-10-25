d227b9d335ac89aca307dbf67aebbad9691f5a75
uri <urialbertogalvafrancisco@gmail.com>
2022-10-25 16:02:34 -0400

    adding navbar and image domain config

--- common/layout/Layout.tsx
+++ common/layout/Layout.tsx
@@ -1,25 +1,23 @@
 import Head from 'next/head'
 import { FC, ReactElement } from 'react'
+import { NavbarPokemon } from '../ui/NavbarPokemon'
 
 type LayoutType = {
-    children: ReactElement | ReactElement[]
-    title: string
+  children: ReactElement | ReactElement[]
+  title: string
 }
 
 export const Layout: FC<LayoutType> = ({ children, title }) => {
-    return (
-        <>
-            <Head>
-                <title>{title || 'PokeApp'}</title>
-                <meta name='author' content='Uris' />
-                <meta name='description' content='info about pokemon' />
-                <meta name='keywords' content='pokemon, pokedex' />
-            </Head>
-
-            <main>
-                {children}
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>{title || 'PokeApp'}</title>
+        <meta name="author" content="Uris" />
+        <meta name="description" content="info about pokemon" />
+        <meta name="keywords" content="pokemon, pokedex" />
+      </Head>
+      <NavbarPokemon />
+      <main>{children}</main>
+    </>
+  )
 }
-
