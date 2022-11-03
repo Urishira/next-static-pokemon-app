896c523807d55342ed5f56704a3cc01fa37e89c2
Uris <54002584+Urishira@users.noreply.github.com>
2022-11-03 17:23:13 -0400

    Revert "adding pokemon adapter to format poke api"
    This reverts commit c470aac0daabc326cd74bebb45c8c5fca231b473.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import type { NextPage } from 'next'
 import { Button } from '@nextui-org/react'
 import { Layout } from '../common/layout'
 import { pokemonApi } from './api'
-import { PokemonApiTypes, SmallPokemon } from '../types'
 
 const Home: NextPage = () => {
   return (
@@ -19,21 +18,23 @@ const Home: NextPage = () => {
 //- The data can be publicly cached (not user-specific).
 //- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
 
+export type Result = {
+  name: string
+  url: string
+}
+
+export type PokemonApiTypes = {
+  count: number
+  next: string
+  previous: any
+  results: Result[]
+}
+
 export const getStaticProps: GetStaticProps = async ctx => {
   const { data } = await pokemonApi.get<PokemonApiTypes>('/pokemon?limit=150')
-  const pokemons: SmallPokemon[] = data.results.map((pokeInfo, i) => {
-    const idPoke = i + 1
-    return {
-      id: idPoke,
-      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${idPoke}.svg`,
-      name: pokeInfo.name,
-      url: `https://pokeapi.co/api/v2/pokemon/${idPoke}/`
-    }
-  })
-
   return {
     props: {
-      pokemons
+      pokemons: data.results
     }
   }
 }
