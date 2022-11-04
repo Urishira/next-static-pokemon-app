6b31ff2b1340dd4e113cb69e898b829b6f2a046c
uri <urialbertogalvafrancisco@gmail.com>
2022-11-04 04:04:31 -0400

    wip fixing card ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,48 @@
 import { GetStaticProps } from 'next'
 import type { NextPage } from 'next'
-import { Button } from '@nextui-org/react'
+import { Button, Grid } from '@nextui-org/react'
 import { Layout } from '../common/layout'
 import { pokemonApi } from './api'
 import { PokemonApiTypes, SmallPokemon } from '../types'
+import { Card, Text, Link } from '@nextui-org/react'
+import Image from 'next/image'
+import { FC } from 'react'
 
-const Home: NextPage = () => {
+type CartPokemonProps = {
+  img: string
+  name: string
+}
+const CardPokemon: FC<CartPokemonProps> = ({ img, name }) => {
+  return (
+    <Card css={{ p: '$6', mw: '400px' }}>
+      <Image alt="nextui logo" src={img} />
+      <Grid.Container css={{ pl: '$6' }}>
+        <Grid xs={12}>
+          <Text h4 css={{ lineHeight: '$xs' }}>
+            Next UI
+          </Text>
+        </Grid>
+        <Grid xs={12}>
+          <Text css={{ color: '$accents8' }}>nextui.org</Text>
+        </Grid>
+      </Grid.Container>
+      <Card.Body css={{ py: '$2' }}>
+        <Text>Make beautiful websites regardless of your design experience.</Text>
+      </Card.Body>
+      <Card.Footer>
+        <Link color="primary" target="_blank" href="https://github.com/nextui-org/nextui">
+          Visit source code on GitHub.
+        </Link>
+      </Card.Footer>
+    </Card>
+  )
+}
+
+const Home: NextPage = props => {
   return (
     <Layout title="Pokemon-Home">
       <Button color="gradient">Hello world</Button>
+      <Grid>{props.pokemon.map()}</Grid>
     </Layout>
   )
 }
