d1f0a7c7b5f983bca0d6eae84e92d75920224500
uri <urialbertogalvafrancisco@gmail.com>
2023-06-18 15:03:28 -0400

    fix: fixing get path by name

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -1,4 +1,4 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import Image from 'next/image'
 import React, { FC } from 'react'
 import { Pokefull, PokemonApiTypes } from '../../types'
@@ -8,7 +8,7 @@ import { pokemonApi } from '../api'
 type PokemonData = {
   pokemon: Pokefull
 }
-const PokemonByName: FC<PokemonData> = ({ pokemon }) => {
+const PokemonByName: NextPage<PokemonData> = ({ pokemon }) => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="grid grid-rows-1 border border-black w-72 h-48">
@@ -24,7 +24,14 @@ const PokemonByName: FC<PokemonData> = ({ pokemon }) => {
 
 export const getStaticPaths: GetStaticPaths = async ctx => {
   const { data } = await pokemonApi.get<PokemonApiTypes>('/pokemon?limit=150')
-  const paths = data.results.map(pokemons => ({ params: { name: pokemons.name } }))
+
+  const PokemonNames = data.results.map(path => path.name)
+  const paths = PokemonNames.map(name => ({
+    params: {
+      name
+    }
+  }))
+
   return {
     paths,
     fallback: false
@@ -32,8 +39,10 @@ export const getStaticPaths: GetStaticPaths = async ctx => {
 }
 
 export const getStaticProps: GetStaticProps = async ctx => {
-  const { name } = ctx.params as unknown as { name: string }
-  const pokemon = getPokemonByParams(name)
+  const { name } = ctx.params as { name: string }
+
+  const pokemon = await getPokemonByParams(name)
+
   return {
     props: {
       pokemon
