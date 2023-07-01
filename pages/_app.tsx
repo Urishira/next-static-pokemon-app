670657a208dd5336cce4d818ffec37d095e253ea
uri <urialbertogalvafrancisco@gmail.com>
2023-07-01 18:09:15 -0400

    feat: nextauth with firebase conf added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { SessionProvider, SessionProviderProps } from 'next-auth/react'
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps<SessionProviderProps>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 export default MyApp
